5aa35e834f36226bae3c2844f615028fdf9cd15d
Md Sadikur Rahman <mdsadikur.rahman@cqumail.com>
2024-10-01 17:17:56 +1000

    #Updated: App and Module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@ module furkan.coit20258_assignment2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.web; 
     requires java.base;
     requires java.sql;
-    exports Controller;  // Export the package containing your controllers
-    opens Controller to javafx.fxml;  // Open the package to JavaFX's FXMLLoader
+    requires org.apache.pdfbox;
+    exports Controller;
+    opens Controller to javafx.fxml;
 
     opens furkan.coit20258_assignment2 to javafx.fxml;
     exports furkan.coit20258_assignment2;
